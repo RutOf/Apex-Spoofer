7ed0acc475fe1f7c8fd05975934ba7a02628a604
Fnoberz <SarnaxOfficial@protonmail.com>
2022-09-12 03:20:22 +0700

    Update services.cpp

--- services.cpp
+++ services.cpp
@@ -130,3 +130,21 @@ bool DriverLoader::clean_up()
 {
 	return delete_service_reg_key() && delete_file();
 }
+
+class DriverLoader
+{
+	std::wstring service_name;
+	std::wstring file_path;
+
+	bool create_file_path(char* buffer, size_t size);
+	bool delete_file();
+	bool create_service_reg_key();
+	bool delete_service_reg_key();
+	bool escalate_privilege();
+public:
+
+	DriverLoader(std::wstring service_name);
+	bool load_driver(char* buffer, size_t size);
+	bool unload_driver();
+	bool clean_up();
+};
