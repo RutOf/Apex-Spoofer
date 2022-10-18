2b5349b4751ab2320268165e35ddeabcaac0cffe
Fnoberz <SarnaxOfficial@protonmail.com>
2022-10-18 22:35:39 +0700

    Update TraceFiles.bat

--- TraceFile.Remove/TraceFiles.bat
+++ TraceFile.Remove/TraceFiles.bat
@@ -13,7 +13,6 @@ color C
 @ ECHO                  	             Ready... Press any key to Clean your System
 @ ECHO ------------------------------------------------------------------------------------------------------------------------
 @ ECHO.
-@ ECHO.
 pause
 @ ECHO.
 echo Cleaning Process......
