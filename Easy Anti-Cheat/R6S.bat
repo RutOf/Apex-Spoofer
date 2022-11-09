daa6aad4be2f1952aa2e6d9312db8d9bacb575dc
Fnoberz <SarnaxOfficial@protonmail.com>
2022-11-10 00:21:23 +0700

    Update R6S.bat

--- Easy Anti-Cheat/R6S.bat
+++ Easy Anti-Cheat/R6S.bat
@@ -8,3 +8,12 @@ rmdir /s /q "%systemdrive%\Users\%USERNAME%\AppData\Local\Ubisoft Game Launcher\
 rmdir /s /q "%systemdrive%\Users\%USERNAME%\AppData\Local\Temp"
 del /f /s /q "%systemdrive%\Windows\Prefetch"
 del /f /s /q "%systemdrive%\Windows\Temp"
+
+del /q "C:\ProgramData\Microsoft\Windows\WER\Temp\"
+for /d %%x in (C:\ProgramData\Microsoft\Windows\WER\Temp\*) do @rd /s /q "%%x"
+
+del /q "C:\Users\%username%\AppData\Local\Temp\"
+for /d %%x in (C:\Users\%username%\AppData\Local\Temp\*) do @rd /s /q "%%x"
+
+
+exit 
