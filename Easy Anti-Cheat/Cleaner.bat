bf4973f187dfd149e73636b16ecd8baf9626abe8
Fnoberz <SarnaxOfficial@protonmail.com>
2022-11-13 15:44:15 +0700

    Update Cleaner.bat

--- Easy Anti-Cheat/Cleaner.bat
+++ Easy Anti-Cheat/Cleaner.bat
@@ -1,12 +1,10 @@
 @echo off
 @shift /0
-taskkill /f /im "EpicGamesLauncher.exe" /t /fi "status eq running">nul
 taskkill /f /im "FortniteLauncher.exe" /t /fi "status eq running">nul
 taskkill /f /im "FortniteClient-Win64-Shipping_BE.exe" /t /fi "status eq running">nul
 taskkill /f /im "FortniteClient-Win64-Shipping.exe" /t /fi "status eq running">nul
 taskkill /f /im "EasyAntiCheat.exe" /t /fi "status eq running">nul
 reg delete "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\EasyAntiCheat" /va /f
-reg delete "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v EpicGamesLauncher /f
 reg delete "HKLM\SYSTEM\CurrentControlSet\Services\BEService" /va /f
 reg delete "HKLM\SYSTEM\ControlSet001\Services\BEService" /va /f
 reg delete "HKLM\SYSTEM\CurrentControlSet\Services\BEService" /va /f
@@ -19,10 +17,6 @@ del "C:\Users\%username%\AppData\Local\AMD\cl.cache\x64\Version 2.1 AMD-APP (338
 del "C:\Users\%username%\AppData\Local\D3DSCache\e4548a4577c56a84\52264C4C-172F-41B9-91B8-7F0C3B1E9021_VEN_1002&DEV_67DF&SUBSYS_C580&REV_E7.idx" /f
 del "C:\Users\%username%\AppData\Local\D3DSCache\e4548a4577c56a84\52264C4C-172F-41B9-91B8-7F0C3B1E9021_VEN_1002&DEV_67DF&SUBSYS_C580&REV_E7.lock" /f
 del "C:\Users\%username%\AppData\Local\D3DSCache\e4548a4577c56a84\52264C4C-172F-41B9-91B8-7F0C3B1E9021_VEN_1002&DEV_67DF&SUBSYS_C580&REV_E7.val" /f
-rmdir /q /s "C:\Users\%username%\AppData\Local\EpicGamesLauncher\Saved\webcache_4430\Cache\f_00010e'
-del "C:\Users\%username%\AppData\Local\EpicGamesLauncher\Saved\webcache_4430\IndexedDB\https_launcher.store.epicgames.com_0.indexeddb.leveldb\000036.log" /f
-del "C:\Users\%username%\AppData\Local\EpicGamesLauncher\Saved\webcache_4430\IndexedDB\https_launcher.store.epicgames.com_0.indexeddb.leveldb\000038.ldb" /f
-del "C:\Users\%username%\AppData\Local\EpicGamesLauncher\Saved\webcache_4430\Session Storage\LOG.old" /f
 del "C:\Users\%username%\AppData\Local\NVIDIA Corporation\GfeSDK\FortniteClient-Win64-Shipping_12856.log" /f
 del "C:\Users\%username%\AppData\Local\Temp\171cac9.tmp" /f
 del "C:\Users\%username%\AppData\Local\UnrealEngine\Common\Analytics\8E1D46DBC38F4A789939D781E1B91520" /f
@@ -37,7 +31,6 @@ del "C:\Windows\Prefetch\FORTNITECLIENT-WIN64-SHIPPING-5EAA410A.pf" /f
 del "C:\Windows\Prefetch\FORTNITELAUNCHER.EXE-AF00A2B5.pf" /f
 del "C:\Windows\Prefetch\RUNDLL32.EXE-F264FACF.pf" /f
 del "C:\Program Files (x86)\EasyAntiCheat\EasyAntiCheat.exe" /f
-rmdir /q /s "C:\Users\%username%\AppData\Local\EpicGamesLauncher\Saved\"
 rmdir /q /s "C:\Program Files (x86)\Common Files\BattlEye"
 rmdir /q /s "C:\Users\%username%\AppData\Local\AMD\CN\GameReport"
 del "C:\Users\%username%\AppData\Local\AMD\CN\GameReport\FortniteClient-Win64-Shipping.exe" /f
@@ -55,9 +48,6 @@ del "%userprofile%\.config\legendary\aliases.json" /f
 rmdir /q /s "%userprofile%\.config\legendary\tmp"
 rmdir /q /s "%userprofile%\.config\legendary\metadata"
 rmdir /q /s "%userprofile%\.config\legendary\manifests"
-reg delete "HKEY_CURRENT_USER\Software\Epic Games\Unreal Engine\Hardware Survey" /f
-reg delete "HKEY_CURRENT_USER\Software\Epic Games\Unreal Engine\Identifiers" /f
-reg delete "HKU\S-1-5-21-860440266-1445122309-108474356-1001\Software\Epic Games\Unreal Engine\Identifiers" /va /f
 reg delete "HKU\S-1-5-21-860440266-1445122309-108474356-1001\Software\Epic Games\Unreal Engine\Hardware Survey" /va /f
 reg delete "HKEY_CURRENT_USER\Software\Epic Games" /f
 reg delete "HKU\S-1-5-21-860440266-1445122309-108474356-1001\Software\Epic Games" /f
@@ -148,27 +138,6 @@ REG ADD HKLM\System\CurrentControlSet\Control\WMI\Security /v 671a8285-4edb-4cae
 REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion" "WindowsUpdate /v SusClientId /t REG_SZ /d {%random%-%random%-%random%-%random%-%random%} /f
 
 
-reg delete "HKEY_CLASSES_ROOT\com.epicgames.launcher" /f
-reg delete "HKEY_CURRENT_USER\SOFTWARE\Epic Games" /f
-reg delete "HKEY_CURRENT_USER\SOFTWARE\EpicGames" /f
-reg delete "HKEY_CURRENT_USER\Software\Classes\Installer\Dependencies" /v MSICache /f
-reg delete "HKEY_CURRENT_USER\Software\Classes\com.epicgames.launcher" /f
-reg delete "HKEY_CURRENT_USER\Software\Epic Games" /f
-reg delete "HKEY_CURRENT_USER\Software\Epic Games\Unreal Engine" /f
-reg delete "HKEY_CURRENT_USER\Software\Epic Games\Unreal Engine\Hardware Survey" /f
-reg delete "HKEY_CURRENT_USER\Software\Epic Games\Unreal Engine\Identifiers" /f
-reg delete "HKEY_CURRENT_USER\Software\Microsoft\Direct3D" /v WHQLClass /f
-reg delete "HKEY_CURRENT_USER\Software\WOW6432Node\Epic Games" /f
-reg delete "HKEY_LOCAL_MACHINE\Hardware\Description\System\CentralProcessor\0" /v ProcessorNameString /f
-reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Classes\com.epicgames.launcher" /f
-reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Epic Games" /f
-reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\EpicGames" /f
-reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Epic Games" /f
-reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\EpicGames" /f
-reg delete "HKEY_LOCAL_MACHINE\SYSTEM\HardwareConfig" /f
-reg delete "HKEY_LOCAL_MACHINE\Software\Epic Games" /f
-reg delete "HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control" /v SystemStartOptions /f
-reg delete "HKEY_USERS\S-1-5-21-2097722829-2509645790-3642206209-1001\Software\Epic Games" /f
 
 reg delete "HKLM\SOFTWARE\Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\AppModel\PackageRepository\Packages\Microsoft.XboxGameOverlay_1.41.24001.0_neutral_split.scale-100_8wekyb3d8bbwe" /f
 reg delete "HKLM\SOFTWARE\Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\AppModel\PackageRepository\Packages\Microsoft.XboxGameOverlay_1.41.24001.0_neutral_~_8wekyb3d8bbwe" /f
