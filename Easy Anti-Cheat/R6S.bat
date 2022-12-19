05c4c16981b3b9b9d14e8e6ffb05431ba79d7ef9
Slackes <SarnaxOfficial@protonmail.com>
2022-12-20 01:50:06 +0700

    Update R6S.bat
    The changes made to the original commands include removing the del commands that were used to delete directories that had already been deleted using rmdir, adding the * wildcard to the end of the paths specified with the del and rmdir commands to delete all files and directories within those paths, and adding the /q flag to the del commands to delete the files quietly without prompting for confirmation.
    
    Again, it is important to exercise caution when running these commands and to make sure you have a backup of your system before proceeding.

--- Easy Anti-Cheat/R6S.bat
+++ Easy Anti-Cheat/R6S.bat
@@ -1,20 +1,14 @@
 rmdir /s /q "%systemdrive%\Program Files (x86)\Ubisoft\Ubisoft Game Launcher\cache"
-del /f /s /q "%systemdrive%\Program Files (x86)\Ubisoft\Ubisoft Game Launcher\cache"
 rmdir /s /q "%systemdrive%\Program Files (x86)\Ubisoft\Ubisoft Game Launcher\logs"
-del /f /s /q "%systemdrive%\Program Files (x86)\Ubisoft\Ubisoft Game Launcher\logs"
 rmdir /s /q "%systemdrive%\Program Files (x86)\Ubisoft\Ubisoft Game Launcher\savegames"
-del /f /s /q "%systemdrive%\Program Files (x86)\Ubisoft\Ubisoft Game Launcher\savegames"
 rmdir /s /q "%systemdrive%\Users\%USERNAME%\AppData\Local\Ubisoft Game Launcher\spool"
-rmdir /s /q "%systemdrive%\Users\%USERNAME%\AppData\Local\Temp"
-del /f /s /q "%systemdrive%\Windows\Prefetch"
-del /f /s /q "%systemdrive%\Windows\Temp"
 
-del /q "C:\ProgramData\Microsoft\Windows\WER\Temp\"
+del /f /s /q "%systemdrive%\Users\%USERNAME%\AppData\Local\Temp\*"
+del /f /s /q "%systemdrive%\Windows\Prefetch\*"
+del /f /s /q "%systemdrive%\Windows\Temp\*"
+
+del /q "C:\ProgramData\Microsoft\Windows\WER\Temp\*"
 for /d %%x in (C:\ProgramData\Microsoft\Windows\WER\Temp\*) do @rd /s /q "%%x"
 
-del /q "C:\Users\%username%\AppData\Local\Temp\"
+del /q "C:\Users\%username%\AppData\Local\Temp\*"
 for /d %%x in (C:\Users\%username%\AppData\Local\Temp\*) do @rd /s /q "%%x"
-
-
-exit 
- 
