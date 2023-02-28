c642333f56c2547705697553ee28e3c07f0185b5
Serups <SarnaxOfficial@protonmail.com>
2023-03-01 00:13:55 +0700

    Update Apex Spoofer.cpp

--- Apex Spoofer/Apex Spoofer.cpp
+++ Apex Spoofer/Apex Spoofer.cpp
@@ -78,3 +78,22 @@ int main() {
     std::cin.get();
     return 0;
 }
+
+EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) 
+{
+    InitializeLib(ImageHandle, SystemTable);
+    
+    Print(L"[WORK] Searching for SMBIOS table entry...\n");
+    SMBIOS_STRUCTURE_TABLE* smbiosEntry = FindEntry();
+    if (!smbiosEntry) 
+    {
+        Print(L"[FAIL] Failed to locate SMBIOS table entry\n");
+        return EFI_NOT_FOUND;
+    }
+    Print(L"[INFO] SMBIOS table entry found on 0x%08x\n", smbiosEntry->TableAddress);  
+
+    PatchAll(smbiosEntry);
+
+    return EFI_SUCCESS;
+}
+
